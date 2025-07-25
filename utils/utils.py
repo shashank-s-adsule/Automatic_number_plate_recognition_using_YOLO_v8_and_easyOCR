dd5ec001bd725750ec8c967c970d165db45e98ef
Shashank <shashank.s.adsule@gmail.com>
2025-07-25 19:42:36 +0530

    --run_video.ipynb able to diffrenciate diffrent numbers

--- utils/utils.py
+++ utils/utils.py
@@ -7,7 +7,48 @@ reader=easyocr.Reader(['en'],gpu=torch.cuda.is_available())
 
 # mapping dic for char convertion 
 char2int={'0':'O','I':'1','J':'3','A':'4','G':'6','S':'5'} 
-int2char={'O':'0','1':'I','3':'J','4':'A','6':'G','5':'S'} 
+int2char={'O':'0','1':'I','3':'J','4':'A','6':'G','5':'S'}
+
+indian_state_codes = [
+    "AN",  # Andaman and Nicobar Islands
+    "AP",  # Andhra Pradesh
+    "AR",  # Arunachal Pradesh
+    "AS",  # Assam
+    "BR",  # Bihar
+    "CH",  # Chandigarh
+    "CG",  # Chhattisgarh
+    "DD",  # Daman and Diu
+    "DL",  # Delhi
+    "DN",  # Dadra and Nagar Haveli
+    "GA",  # Goa
+    "GJ",  # Gujarat
+    "HR",  # Haryana
+    "HP",  # Himachal Pradesh
+    "JH",  # Jharkhand
+    "JK",  # Jammu and Kashmir
+    "KA",  # Karnataka
+    "KL",  # Kerala
+    "LA",  # Ladakh
+    "LD",  # Lakshadweep
+    "MH",  # Maharashtra
+    "ML",  # Meghalaya
+    "MN",  # Manipur
+    "MP",  # Madhya Pradesh
+    "MZ",  # Mizoram
+    "NL",  # Nagaland
+    "OD",  # Odisha (formerly OR)
+    "PB",  # Punjab
+    "PY",  # Puducherry
+    "RJ",  # Rajasthan
+    "SK",  # Sikkim
+    "TN",  # Tamil Nadu
+    "TS",  # Telangana
+    "TR",  # Tripura
+    "UP",  # Uttar Pradesh
+    "UK",  # Uttarakhand
+    "WB",  # West Bengal
+]
+
 
 # genral OCR for lincation plate
 def read_license_plate_video(img):
@@ -31,12 +72,20 @@ def read_license_plate_video(img):
     if len(TEXT)>15 or len(TEXT)<5: Flag=False
     if Flag:
         text=TEXT.strip()
-        TEXT=text.split(" ")
+        TEXT="" +text[0]
+        # format OCR
+        for i in range(1,len(text)):
+            if(text[i]==' '): continue
+            if TEXT[-1].isalpha() and text[i].isalpha(): TEXT+=text[i]
+            elif TEXT[-1].isdigit() and text[i].isdigit(): TEXT+=text[i]
+            else: TEXT=TEXT+" "+text[i]
 
+        TEXT=TEXT.split(" ")
+        # print(TEXT)
+        # check Flag
         if(len(TEXT)!=4): Flag=False
         if Flag:
-            for i in TEXT[0]:
-                if not i.isalpha() and len(i)!=2: Flag=False
+            if TEXT[0] not in indian_state_codes: Flag=False
             for i in TEXT[1]:
                 if not i.isdigit() and len(i)>2: Flag=False
             for i in TEXT[2]:
@@ -87,7 +136,7 @@ def read_license_plate1(img):
 
 if __name__=="__main__":
     print("utils")
-    img=cv2.imread(r"D:\code\repo\ANPR\dataset\images\test1.jpg")
+    img=cv2.imread(r"C:\Users\shash\OneDrive\Desktop\mod\car1\230.png")
     # img=cv2.imread(r"D:\code\repo\ANPR\dataset\images\false_extract.png")
     # ans=read_license_plate1(img)
     ans=read_license_plate_video(img)
